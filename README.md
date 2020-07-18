3c920046727e35bb1bd6b15f8c8ca329cd17a88d
umanari145 <umanari145@gmail.com>
2020-07-18 11:40:09 +0900

    マイグレーションツールphinxの使い方について

--- README.md
+++ README.md
@@ -5,9 +5,33 @@ cakephp2.10 + postgres
 
 # migrationsについて
 
+## CakeDC
+- 履歴はDB管理
+- git submodule(特定のディレクトりを別のgitの範囲にする)
+- かなり複雑で面倒っぽい
+- 直SQLは無理っぽい
+- 複合主キー無理(cake2の仕様がそうらしい)
 参考<br>
 https://www.ryuzee.com/contents/blog/6108
 
 
+## Dbup 
+- 超簡単
+- 履歴をテキスト管理
+- upコマンドのみ
+- SQLのなかに;を入れるとダメ
 
+参考<br>
+https://brtriver.github.io/dbup/ja/
+
+## phinx
+- 直SQL可能
+- 履歴はDB管理
+- ロールバックあり
+- 特定のマイグレーションのみ実行可能
+- シーダーなども可能
+
+参考<br>
+https://phinx.org/
+https://qiita.com/hypermkt/items/b915b8a9fbda2f0c612e
 
---
+++ db/migrations/20200718023344_create_sample_table.php
@@ -0,0 +1,66 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateSampleTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $sql =<<<EOF
+CREATE TABLE persons (
+    id  SERIAL PRIMARY KEY,
+    person_name varchar(50) DEFAULT '',
+    sex smallint DEFAULT NULL,
+    birth_day date DEFAULT NULL,
+    zip varchar(7)  DEFAULT '',
+    address_code varchar(15) DEFAULT '',
+    address1 varchar(100) DEFAULT '',
+    address2 varchar(100) DEFAULT '',
+    contact smallint DEFAULT NULL,
+    email varchar(200) DEFAULT '',
+    tel varchar(15) DEFAULT '',
+    delivery_zip varchar(7) DEFAULT '',
+    delivery_address1 varchar(100) DEFAULT '',
+    delivery_address2 varchar(100) DEFAULT '',
+    traffic json DEFAULT NULL,
+    contents text,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
+    
+    
+    INSERT INTO persons (person_name, sex) VALUES 
+    ('山田太郎',1);
+    INSERT INTO persons (person_name, sex) VALUES 
+    ('山田花子',2);
+EOF;
+
+        $this->execute($sql);
+
+    }
+}
