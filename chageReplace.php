7cff76fc135872836cf73777597d56ccf7e0a26f
umanari145 <umanari145@gmail.com>
2020-09-24 23:58:11 +0900

    ファイル置き換え

--- chageReplace.php
+++ chageReplace.php
@@ -1,5 +1,24 @@
 <?php
 
+$file_path = __DIR__. "/app/Config/bootstrap.php";
+$configBefore = file_get_contents($file_path);
 
-$debugPatter = 
-preg_replace();
+$debugPattern = "/CakeLog::config\('debug',(.*?)'engine' => 'ConsoleLog'(.*?)'stream' => new ConsoleOutput\(\)(.*?)\)/s";
+$debugReplace = "CakeLog::config('debug',$1'engine' => 'File'$2'file' => 'debug'$3)";
+
+$errorPattern = "/CakeLog::config\('error',(.*?)'engine' => 'ConsoleLog'(.*?)'stream' => new ConsoleOutput\('php:\/\/stderr'\)(.*?)\)/s";
+$errorReplace = "CakeLog::config('error',$1'engine' => 'File'$2'file' => 'error'$3)";
+
+$pattenArr = [
+    $debugPattern,
+    $errorPattern
+];
+
+$replaceArr = [
+    $debugReplace,
+    $errorReplace
+];
+
+
+$configAfter = preg_replace($pattenArr, $replaceArr, $configBefore);
+file_put_contents($file_path, $configAfter);
